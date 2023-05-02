2307bb450b2a5ea3e90d28394cb03589cabbf4c2
Clemens Sageder <clemenssageder@icloud.com>
2023-05-02 14:39:12 +0200

    Initial Commit

---
+++ Otis/OtisApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtisApp.swift
+//  Otis
+//
+//  Created by Clemens Sageder on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct OtisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
